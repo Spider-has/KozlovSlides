18362b19002070385a844130ce7d8a3740838009
Spider-has <romanh998@gmail.com>
2023-10-25 20:37:27 +0300

    Настроил линтеры

--- front/model/types.ts
+++ front/model/types.ts
@@ -1,136 +1,163 @@
 type Id = string;
 
 type Span = {
-    value: string,
-    fontFamily: string,
-    fontSize: number,
-    color: string,
-    bold: boolean,
-    cursive: boolean,
-    underline: boolean,
-}
+   value: string;
+   fontFamily: string;
+   fontSize: number;
+   color: string;
+   bold: boolean;
+   cursive: boolean;
+   underline: boolean;
+};
 
 type StrokeObject = {
-    type: "text",
-    chars: Array<Span>,
-    rotateAngle: number,
-    border: Border
-}
+   type: 'text';
+   chars: Array<Span>;
+   rotateAngle: number;
+   border?: Border;
+};
 
 type GraphicObject = {
-    type: "graphic",
-    color: string,
-    border: Border,
-    rotateAngle: number,
-    opacity: number,
-    figureType: EllipseObject | TriangleObject | RectangleObject,
-    textBlock?: StrokeObject 
-}
+   type: 'graphic';
+   color: string;
+   border?: Border;
+   rotateAngle: number;
+   opacity: number;
+   figureData: EllipseObject | TriangleObject | RectangleObject;
+   textBlock?: StrokeObject;
+};
 
 type EllipseObject = {
-    type: "ellipse",
-    size: Size,
-    rounding: number
-}
+   type: 'ellipse';
+   size: Size;
+   rounding: number;
+};
 
 type TriangleObject = {
-    type: "triangle",
-    point1: Point,
-    point2: Point,
-    point3: Point,
-}
+   type: 'triangle';
+   point1: Point;
+   point2: Point;
+   point3: Point;
+};
+
 type Point = {
-    x: number,
-    y: number
-}
+   x: number;
+   y: number;
+};
+
 type RectangleObject = {
-    type: "rectangle",
-    rounding: number,
-    size: Size,
-}
+   type: 'rectangle';
+   rounding: number;
+   size: Size;
+};
 
 type ImageObject = {
-    type: "image",
-    size: Size,
-    imgUrl: string,
-}
-
+   type: 'image';
+   size: Size;
+   imgUrl: string;
+};
 
 type VideoObject = {
-    type: "video",
-    size: Size,
-    videoUrl: string,
-}
+   type: 'video';
+   size: Size;
+   videoUrl: string;
+};
 
 type AudioObject = {
-    type: "audio",
-    size: Size,
-    audioUrl: string,
-}
+   type: 'audio';
+   size: Size;
+   audioUrl: string;
+};
 
-type FuncGraphObject = {
-    type: "functionGraph",
-    size: Size,
-}
+type FunctionGraphObject = {
+   type: 'functionGraph';
+   size: Size;
+   func: string;
+};
 
 type Border = {
-    color: string,
-    width: number,
-    type: 'solid' | 'dashed'
-}
-
+   color: string;
+   width: number;
+   type: 'solid' | 'dashed';
+};
 
 type SlideElement = {
-    id: Id,
-    position: Point,
-    type: StrokeObject | GraphicObject | ImageObject | VideoObject | AudioObject | FuncGraphObject
-}
+   id: Id;
+   position: Point;
+   data:
+      | StrokeObject
+      | GraphicObject
+      | ImageObject
+      | VideoObject
+      | AudioObject
+      | FunctionGraphObject;
+};
 
 type SlideTransion = {
-    type: string
-}
+   type: string;
+};
 
 type ElementAnimation = {
-    type: string,
-    id: Id
-}
+   type: string;
+   id: Id;
+};
 
 type SlideBackground = {
-    type: "image" | "color",
-    data: backgroundImg | backgroundColor,
-}
+   type: 'image' | 'color';
+   data: backgroundImg | backgroundColor;
+};
 
 type backgroundImg = {
-    url: string
-}
+   url: string;
+};
 
 type backgroundColor = {
-    color: string
-}
+   color: string;
+};
 
 type Slide = {
-    id: Id,
-    elements: Array<SlideElement>,
-    transitionAnimation: SlideTransion,
-    selectedElements: Array<Id>,
-    elementsAnimations: Array<ElementAnimation>,
-    background: SlideBackground,
-}
+   id: Id;
+   elements: Array<SlideElement>;
+   transitionAnimation?: SlideTransion;
+   selectedElements: Array<Id>;
+   elementsAnimations: Array<ElementAnimation>;
+   background: SlideBackground;
+};
 
 type Size = {
-    width: number,
-    height: number,
-}
+   width: number;
+   height: number;
+};
 
 type Presentation = {
-    slides: Array<Slide>,
-    size: Size,
-    name: string   
-}
+   slides: Array<Slide>;
+   size: Size;
+   name: string;
+};
 
 type Editor = {
-    presentation: Presentation,
-    history: Array<Presentation>,
-    selectedSlides: Array<Id>,
-    viewMode: 'edit' | 'view'
-}
+   presentation: Presentation;
+   history: Array<Presentation>;
+   selectedSlides: Array<Id>;
+   viewMode: 'edit' | 'view';
+};
+
+export {
+   Span,
+   StrokeObject,
+   GraphicObject,
+   EllipseObject,
+   RectangleObject,
+   TriangleObject,
+   Slide,
+   Presentation,
+   Editor,
+   ImageObject,
+   VideoObject,
+   AudioObject,
+   ElementAnimation,
+   SlideElement,
+   FunctionGraphObject as FuncGraphObject,
+};
+
+console.log();
