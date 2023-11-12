7f4fb1a83042974b07e87b85100a9e7663a29ca0
Spider-has <romanh998@gmail.com>
2023-11-12 16:02:28 +0300

    icons

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,5 +1,21 @@
+import { CopyFormatting } from './icons/CopyFormatting';
+import { Delete } from './icons/Delete';
+import { Download } from './icons/Download';
+import { Insert } from './icons/Insert';
+import { Redo } from './icons/Redo';
+import { Undo } from './icons/Undo';
 const Button = () => {
-    return <div>RandomButton</div>;
+    return (
+        <div>
+            RandomButton
+            <Delete />
+            <Download />
+            <Insert />
+            <CopyFormatting />
+            <Undo />
+            <Redo />
+        </div>
+    );
 };
 
 export { Button };
