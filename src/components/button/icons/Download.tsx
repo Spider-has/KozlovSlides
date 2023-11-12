bee1fb0aa7a1578ab8f753a35e7aaae87d860a42
Иван Березин <ivberolymp@yandex.ru>
2023-11-12 16:24:27 +0300

    ne sosiska
    ыыыыыы

---
+++ src/components/button/icons/Download.tsx
@@ -0,0 +1,10 @@
+const Download = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px">
+        <path
+            xmlns="http://www.w3.org/2000/svg"
+            fill="#444746"
+            d="M13 8l-1.06-1.06-2.19 2.19V2h-1.5v7.13L6.06 6.94 5 8l4 4zm.5 4v1.5h-9V12H3v1.5c0 .83.67 1.5 1.5 1.5h9c.83 0 1.5-.67 1.5-1.5V12h-1.5z"
+        />
+    </svg>
+);
+export { Download };
