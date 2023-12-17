65455682e62b614b0a222afec4faf4d327d0579a
Spider-has <romanh998@gmail.com>
2023-12-17 22:09:50 +0300

    Первая реализация dnd(((
    херня вышла

--- src/components/editSlideArea/EditSlideArea.tsx
+++ src/components/editSlideArea/EditSlideArea.tsx
@@ -6,6 +6,7 @@ import {
     SlideElement,
 } from '../../model/figureTypes';
 import './EditSlideArea.css';
+import { useEffect, useRef } from 'react';
 
 const SlideEditSpace = (props: { slide: Slide }) => {
     return (
@@ -13,8 +14,7 @@ const SlideEditSpace = (props: { slide: Slide }) => {
             {props.slide.id}
             <div className="main-edit-slide-space">
                 <svg
-                    width="200"
-                    height="250"
+                    className="svg-space"
                     version="1.1"
                     xmlns="http://www.w3.org/2000/svg"
                 >
@@ -27,64 +27,108 @@ const SlideEditSpace = (props: { slide: Slide }) => {
 
 const SlideObject = (props: SlideElement) => {
     const elem = { ...props };
+    let Obj = <></>;
     switch (elem.elementType) {
         case ObjectType.Text: {
-            return;
+            Obj = <></>;
+            break;
         }
         case ObjectType.Graphic: {
             switch (elem.figureType) {
                 case FigureObjects.Rectangle: {
-                    return <Rectangle {...elem} />;
+                    Obj = <Rectangle {...elem} />;
+                    console.log(1);
+                    break;
                 }
-                case FigureObjects.Triangle:
-                    return;
+                case FigureObjects.Triangle: {
+                    Obj = <></>;
+                    break;
+                }
                 case FigureObjects.Ellipse: {
-                    return;
+                    Obj = <></>;
+                    break;
                 }
                 default:
-                    return;
+                    Obj = <></>;
+                    break;
             }
+            break;
         }
         case ObjectType.Image: {
-            return;
+            Obj = <></>;
+            break;
         }
         case ObjectType.Video: {
-            return;
+            Obj = <></>;
+            break;
         }
         case ObjectType.Audio: {
-            return;
+            Obj = <></>;
+            break;
         }
         case ObjectType.FunctionGraph: {
-            return;
+            Obj = <></>;
+            break;
         }
-        default:
-            return;
+        default: {
+            Obj = <></>;
+        }
     }
+
+    return <g>{Obj}</g>;
 };
 
-// type OnDragStartFunc = {
-//     onDragAction: () => void;
-//     onDropAction: () => void;
-// };
+type OnDragStartFunc = {
+    onDragAction: () => void;
+    onDropAction: () => void;
+};
 
-// const useDragAndDrop = (ref, Actions: OnDragStartFunc) => {
-//     useEffect(() => {
-//         console.log('initDragAndDrop');
-//         document.addEventListener('mousedown', e => {
-//             // const startPosition = {
-//             //     x: e.pageX,
-//             //     y: e.pageY,
-//             // };
-//             document.addEventListener('mousemove', () => {
-//                 console.log('move');
-//                 Actions.onDragAction();
-//             });
-//             document.addEventListener('mouseup', () => {
-//                 Actions.onDropAction();
-//             });
-//         });
-//     }, []);
-// };
+const useDragAndDrop = (
+    ref: React.RefObject<SVGGElement>,
+    startElemPos: { x: number; y: number },
+    Actions: OnDragStartFunc,
+) => {
+    useEffect(() => {
+        const startMousePosition = {
+            x: 0,
+            y: 0,
+        };
+        const onMouseUp = () => {
+            console.log('up');
+            Actions.onDropAction();
+            ref.current!.style.position = '';
+            ref.current!.style.zIndex = '';
+            ref.current!.style.border = '';
+            ref.current!.style.left = '';
+            ref.current!.style.top = '';
+            document.removeEventListener('mousemove', onMouseMove);
+            document.removeEventListener('mouseup', onMouseUp);
+        };
+
+        const onMouseMove = (e: MouseEvent) => {
+            ref.current!.style.zIndex = '1';
+            ref.current!.style.border = '2px solid black';
+            ref.current!.setAttribute(
+                'x',
+                startElemPos.x + e.pageX - startMousePosition.x + 'px',
+            );
+
+            ref.current!.setAttribute(
+                'y',
+                startElemPos.y + e.pageY - startMousePosition.y + 'px',
+            );
+            Actions.onDragAction();
+        };
+
+        console.log('initDragAndDrop');
+        ref.current!.addEventListener('mousedown', (e: MouseEvent) => {
+            startMousePosition.x = e.pageX;
+            startMousePosition.y = e.pageY;
+            document.addEventListener('mousemove', onMouseMove);
+            document.addEventListener('mouseup', onMouseUp);
+        });
+    }, []);
+};
 
 const AllObjects = (props: { slideElements: SlideElement[] }) => {
     const objects = props.slideElements.map((elem, i) => {
@@ -95,8 +139,22 @@ const AllObjects = (props: { slideElements: SlideElement[] }) => {
 
 const Rectangle = (props: RectangleElement) => {
     const elem = { ...props };
+    const ref = useRef<SVGRectElement>(null);
+    useDragAndDrop(
+        ref,
+        {
+            x: elem.position.x,
+            y: elem.position.y,
+        },
+        {
+            onDragAction: () => {},
+            onDropAction: () => {},
+        },
+    );
+
     return (
         <rect
+            ref={ref}
             x={elem.position.x}
             y={elem.position.y}
             width={elem.properties.size.width}
